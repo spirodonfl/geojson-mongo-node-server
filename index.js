8c0200a73f28ec4d8282b0afb5b97b393eee178d
Spiro Floropoulos <spirodonfl@gmail.com>
2018-12-09 23:29:28 -0800

    Setting up ability to add a point

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ console.log(mongoConnectionURL);
 
 let ss = new nss.SimpleServer(process.env.SERVER_PORT, './');
 ss.addRoute('POST', '/point', (request, response) => {
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Request-Method', '*');
+    response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
+    response.setHeader('Access-Control-Allow-Headers', '*');
     let arrival = [];
     request
         .on('data', (chunk) => { arrival.push(chunk); })
