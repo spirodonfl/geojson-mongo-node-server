c84f7b92a6475b28999dd0b415cad2753cbc1ee8
Spiro Floropoulos <spirodonfl@gmail.com>
2018-12-10 00:27:58 -0800

    Setting up ability to add a point

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ ss.addRoute('POST', '/point', (request, response) => {
             mongoose.connect(mongoConnectionURL, {useNewUrlParser: true});
             let db = mongoose.connection;
             db.on('error', console.error.bind(console, 'connection error:'));
-            db.once('open', function() {
+            db.once('open', () => {
                 var TestSchema = new mongoose.Schema({
                     title: String,
                     test: {},
@@ -63,7 +63,7 @@ ss.addRoute('POST', '/point', (request, response) => {
                     } else {
                         console.log('New document', document);
                         db.close();
-                        let shipment = {ok: true};
+                        let shipment = {ok: true, _id: document._id};
                         response.writeHead(200, { 'Content-Type': 'application/json' });
                         response.end(JSON.stringify(shipment));
                         return true;
@@ -88,6 +88,61 @@ ss.addRoute('POST', '/point', (request, response) => {
                 //     console.log(kittens);
                 // });
                 // Kitten.find({ name: /^fluff/ }, callback);
+            });
+        });
+});
+ss.addRoute('POST', '/point', (request, response) => {
+    console.log('Point request!');
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Request-Method', '*');
+    response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
+    response.setHeader('Access-Control-Allow-Headers', '*');
+    let arrival = [];
+    request
+        .on('data', (chunk) => { arrival.push(chunk); })
+        .on('end', () => {
+            arrival = Buffer.concat(arrival).toString();
+            arrival = JSON.parse(arrival);
+            console.log('arrival: ', arrival);
+
+            console.log(mongoConnectionURL);
+            let mongoose = require('mongoose');
+            mongoose.connect(mongoConnectionURL, {useNewUrlParser: true});
+            let db = mongoose.connection;
+            db.on('error', console.error.bind(console, 'connection error:'));
+            db.once('open', () => {
+                var TestSchema = new mongoose.Schema({
+                    title: String,
+                    test: {},
+                    point: mongoose.Schema.Types.Point,
+                    multipoint: mongoose.Schema.Types.MultiPoint,
+                    linestring: mongoose.Schema.Types.LineString,
+                    multilinestring: mongoose.Schema.Types.MultiLineString,
+                    polygon: mongoose.Schema.Types.Polygon,
+                    multipolygon: mongoose.Schema.Types.MultiPolygon,
+                    geometry: mongoose.Schema.Types.Geometry,
+                    geometrycollection: mongoose.Schema.Types.GeometryCollection,
+                    feature: mongoose.Schema.Types.Feature,
+                    featurecollection: mongoose.Schema.Types.FeatureCollection
+                }, { typeKey: '$type', collection: 'echoes' });
+                var GeoJSON = db.model('GeoJSON', TestSchema);
+                GeoJSON.find({}, (error, documents) => {
+                    if (error) {
+                        console.error(error);
+                        db.close();
+                        let shipment = {ok: false};
+                        response.writeHead(200, { 'Content-Type': 'application/json' });
+                        response.end(JSON.stringify(shipment));
+                        return false;
+                    } else {
+                        console.log('Documents found', documents);
+                        db.close();
+                        let shipment = {ok: true, documents};
+                        response.writeHead(200, { 'Content-Type': 'application/json' });
+                        response.end(JSON.stringify(shipment));
+                        return true;
+                    }
+                });
             });
         });
 });
